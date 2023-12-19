7d596a84e6ec1959cf5a4fea90e30b6386e6c899
sathvika jakkula <147492164+sathvika-jakkula@users.noreply.github.com>
2023-12-19 22:05:42 +0530

    Add files via upload

--- bestfit.c
+++ bestfit.c
@@ -3,13 +3,13 @@
 int main(){
 	int p,m,i,j,temp,z;
 	int process[10],flag[10],memory[10],allocate[10];
-	printf("enter p");
+	printf("");
 	scanf("%d",&p);
 	for(i=1;i<=p;i++){
 		scanf("%d",&process[i]);
 		allocate[i]=-1;
 	}
-	printf("enter m");
+	printf("");
 	scanf("%d",&m);
 	for(i=1;i<=m;i++){
 		scanf("%d",&memory[i]);
@@ -20,17 +20,24 @@ int main(){
 	for(i=1;i<=p;i++){
 		z=-1;
 		for(j=1;j<=m;j++){
+			if(flag[j]==0){
+		
 			if( process[i]<=memory[j]){
-				if(z==-1){
-					z=j;
-				}else if(memory[z]>memory[j]){
-					z=j;
+					if(z==-1){
+						z=j;
+						
+					}else if(memory[z]>memory[j]){
+						z=j;
+					
+					}
 				}
-			}
+				}
 		}
 		if(z!=-1){
 			allocate[i]=z;
 			memory[z]=memory[z]-process[i];
+			flag[z]=1;
+//			flag[i]=1;
 		}
 	}
 	for(i=1;i<=p;i++){
